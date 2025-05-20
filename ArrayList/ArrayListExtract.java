75999b2c80a3b5ea8de6ebcc100851caa6ad7542
Adhyashetty-bit <shettyadhya40@gmail.com>
2025-05-20 23:46:55 +0530

    Add files via upload

---
+++ ArrayList/ArrayListExtract.java
@@ -0,0 +1,25 @@
+/*Write a java program for getting different colors through ArrayList interface and extract the
+elements 1st and 2nd from the ArrayList object by using SubList()*/
+package ArrayList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class ArrayListExtract {
+	
+public static void main(String[] args) {
+	        ArrayList<String> colors = new ArrayList<>();
+	           colors.add("Blue");
+	   colors.add("Green");
+	   colors.add("Red");
+	   colors.add("Yellow");
+	   colors.add("Black");
+	   
+
+	   List<String> subList = colors.subList(0, 2);
+	   
+
+	   System.out.println("Extracted elements: " + subList);
+	}
+	}
+
+
